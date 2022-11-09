a96442df84ae5cffb033b4ace0a2b197742f2189
Julian Salazar <julian.salazarlpz@proton.me>
2022-11-09 16:44:19 -0600

    Initial push

---
+++ src/main/java/com/github/jj_sallo/recordstore/RecordStoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.jj_sallo.recordstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jj_sallo/recordstore/RecordStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jj_sallo.recordstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
